5b11817b290ca7b35f9eb00667e773a0639e678c
ahmed-shami <ahmedmshami@outlook.com>
2026-04-24 23:54:47 -0400

    Initial Commit

---
+++ NutriGuard_App/NutriGuard_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriGuard_AppApp.swift
+//  NutriGuard_App
+//
+//  Created by Ahmed Shami on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct NutriGuard_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
